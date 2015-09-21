802e0bd24afde124d16ebb6d057ed9e29427d7c0
Wojciech Sznapka <wojciech@sznapka.pl>
2015-09-21 23:03:26 +0200

    Adding new schedule

--- app/scripts/controllers/schedule.js
+++ app/scripts/controllers/schedule.js
@@ -16,14 +16,42 @@ angular.module('pihomeApp')
                 allowWeek: true
             }
         };
+
+        $scope.switches = {
+            heating_ground_floor: 'Podłogówka parter',
+            heating_first_floor: 'Podłogówka piętro',
+            hot_water_pump: 'Pompa CWU',
+            light_stairs: 'Lampa schody',
+            light_hall: 'Lampa hall'
+        };
+
+        $scope.newPrototype = {
+            switch: 'hot_water_pump',
+            state: '1',
+            duration: 30,
+            multiplier: '1',
+            schedule: null
+        };
+        $scope.new = angular.copy($scope.newPrototype);
+
         $scope.loadSchedules =  function load () {
-            $scope.schedules =Schedule.get(function ok (data) {}, function fail(error) {
+            $scope.schedules = Schedule.get(function ok (data) {
+                $scope.adding = false;
+                $scope.new = angular.copy($scope.newPrototype);
+            }, function fail(error) {
                 $window.alert(error.statusText ? error.statusText : 'Connection problem');
+                $scope.adding = false;
             });
         };
 
         $scope.addSchedule = function add() {
-            $scope.schedules.push({cron: '1 */5 * * 200', task: 'baz'});
+            $scope.adding = true;
+            var schedule = new Schedule($scope.new);
+            schedule.duration = schedule.duration * schedule.multiplier;
+            delete schedule.multiplier;
+            schedule.$save(function ok () {
+                $scope.loadSchedules();
+            });
         };
 
         $scope.removeSchedule = function remove(entry) {
