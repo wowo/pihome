46a57e22a9ba5c196d16b79216284c4d8f7793b5
Wojciech Sznapka <wojciech@sznapka.pl>
2016-03-08 00:04:26 +0100

    Sensors chart - dynamic series

--- app/scripts/controllers/sensors.js
+++ app/scripts/controllers/sensors.js
@@ -9,6 +9,20 @@
  */
 angular.module('pihomeApp')
     .controller('SensorCtrl', function ($scope, Reading) {
+        $scope.options = {
+            axes: {
+                x: {type: 'date', key: 'x'},
+                y: {type: 'linear'}
+            },
+            series: [],
+            lineMode: 'linear',
+            tooltip: {mode: 'axes', formatter: function(x, y) {return moment(x).fromNow() + ' : ' + y;}},
+            drawLegend: true,
+            drawDots: false,
+            columnsHGap: 5,
+            tension: 0.7
+        };
+
         $scope.maxDate = moment().format('MM-DD-YYYY');
         $scope.until = $scope.maxDate;
         $scope.since = moment().subtract(1, 'days').format('MM-DD-YYYY');
@@ -18,24 +32,22 @@ angular.module('pihomeApp')
                 data._embedded.forEach(function (element) {
                     element.x = moment(element.x);
                 });
+                if (!$scope.options.series.length) {
+                    _.forEach(data._embedded[0], function(k, v) {
+                        if ('x' !== v) {
+                            $scope.options.series.push({
+                                y: v,
+                                id: v,
+                                thickness: '1px',
+                                type: 'line',
+                                label: _.startCase(v),
+                                drawDots: true,
+                                dotSize: 2
+                            });
+                        }
+                    })
+                }
                 $scope.data = data._embedded;
             });
         });
-
-        $scope.options = {
-            axes: {
-                x: {type: 'date', key: 'x'},
-                y: {type: 'linear'}
-            },
-            series: [
-                {y: 'outdoor-north', id: 'outdoor-north', thickness: '1px', type: 'line', label: 'Zewnątrz północ', drawDots: true, dotSize: 2},
-                {y: 'office2', id: 'office2', thickness: '1px', type: 'line', label: 'Gabinet', drawDots: true, dotSize: 2},
-            ],
-            lineMode: 'linear',
-            tooltip: {mode: 'axes', formatter: function(x, y) {return moment(x).fromNow() + ' : ' + y;}},
-            drawLegend: true,
-            drawDots: false,
-            columnsHGap: 5,
-            tension: 0.7
-        };
     });
